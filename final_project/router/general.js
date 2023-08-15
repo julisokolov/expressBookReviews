dbacfec2fc6c05022af96f581481621ae6abd6bb
Juli Sokolov <julisokolov13@gmail.com>
2023-08-15 15:56:12 -0400

    first few tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,25 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const bookKeys = Object.keys(books)
+    let authorBooks = []
+    for (key in bookKeys) {
+        if (books[key]["author"] == req.params.author) {
+            authorBooks.concat(authorBooks, books[key])
+        }
+    }
+    return res.send(authorBooks);
 });
 
 // Get all books based on title
