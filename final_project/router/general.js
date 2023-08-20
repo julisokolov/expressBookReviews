794e0b76d97c63d3431405dcf6bab644a90d41bb
Juli Sokolov <julisokolov13@gmail.com>
2023-08-20 15:47:12 -0400

    didnt finish exercise 7

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,22 +4,11 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const doesExist = (username)=>{
-    let userswithsamename = users.filter((user)=>{
-      return user.username === username
-    });
-    if(userswithsamename.length > 0){
-      return true;
-    } else {
-      return false;
-    }
-};
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
     if (username && password) {
-      if (!doesExist(username)) {
+      if (!isValid(username)) {
         users.push({"username":username,"password":password});
         return res.status(200).json({message: "User successfully registred. Now you can login"});
       } else {
@@ -27,7 +16,7 @@ public_users.post("/register", (req,res) => {
       }
     }
     return res.status(404).json({message: "Unable to register user."});
-  });
+});
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
